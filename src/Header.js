7e5e1ec8e87fa613c7015c14020bf25fcf11eb7c
Dalton <Daltonjavery@gmail.com>
2022-02-16 00:11:53 -0600

    updated href values

--- src/Header.js
+++ src/Header.js
@@ -6,9 +6,9 @@ function Header() {
     return (
         <div className='header'>
             <ul className='header-list'>
-                <li><h2><a href=''>Home</a></h2></li>
-                <li><h2><a href=''>Road Map</a></h2></li>
-                <li><h2><a href=''>About</a></h2></li>
+                <li><h2><a href='/'>Home</a></h2></li>
+                <li><h2><a href='/'>Road Map</a></h2></li>
+                <li><h2><a href='/'>About</a></h2></li>
             </ul>
         </div>
     )
